b1effb9d8fa0947a1716f614de74364c40357795
Mohammad Tanvir Islam <tanvieer@hotmail.com>
2018-03-24 03:02:16 +0600

    login page design complete

--- ReactPlanteenView/src/index.js
+++ ReactPlanteenView/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 import {createBrowserHistory} from 'history';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 // Styles
 // Import Font Awesome Icons Set
@@ -22,14 +23,41 @@ import Page500 from './views/Pages/Page500/'
 
 const history = createBrowserHistory();
 
+// const App = () => (
+//   <MuiThemeProvider>
+//     <MyAwesomeReactComponent />
+//   </MuiThemeProvider>
+// );
+
+
 ReactDOM.render((
-  <HashRouter history={history}>
-    <Switch>
-      <Route exact path="/login" name="Login Page" component={Login}/>
-      <Route exact path="/register" name="Register Page" component={Register}/>
-      <Route exact path="/404" name="Page 404" component={Page404}/>
-      <Route exact path="/500" name="Page 500" component={Page500}/>
-      <Route path="/" name="Home" component={Full}/>
-    </Switch>
-  </HashRouter>
+    <MuiThemeProvider>
+    <HashRouter history={history}>
+      <Switch>
+        <Route exact path="/login" name="Login Page" component={Login}/>
+        <Route exact path="/register" name="Register Page" component={Register}/>
+        <Route exact path="/404" name="Page 404" component={Page404}/>
+        <Route exact path="/500" name="Page 500" component={Page500}/>
+        <Route path="/" name="Home" component={Full}/>
+      </Switch>
+    </HashRouter>
+    </MuiThemeProvider>
 ), document.getElementById('root'));
+
+
+//
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+//
+// const App = () => (
+//   <MuiThemeProvider>
+//     <MyAwesomeReactComponent />
+//   </MuiThemeProvider>
+// );
+//
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('app')
+// );
--- ReactPlanteenView/src/views/Pages/Login/Login.js
+++ ReactPlanteenView/src/views/Pages/Login/Login.js
@@ -1,46 +1,91 @@
 import React, {Component} from "react";
 import {Container, Row, Col, CardGroup, Card, CardBlock, Button, Input, InputGroup, InputGroupAddon} from "reactstrap";
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import {white500, blue500, red500, grey300} from 'material-ui/styles/colors';
+
+const styles = {
+  errorStyle: {
+    color: red500,
+  },
+  underlineStyle: {
+    borderColor: white500,
+  },
+  floatingLabelStyle: {
+    color: white500,
+  },
+  floatingLabelFocusStyle: {
+    color: white500,
+  },
+  hintStyle : {
+    color: grey300,
+  },
+  loginButtonStyle: {
+     marginLeft: 40,
+     marginTop: 20,
+     textTransform: 'uppercase',
+  },
+  linkStyle: {
+    textDecorationLine: 'underline',
+    marginLeft: 10,
+    fontSize: 13,
+  },
+  customBackgroundImage:{
+    backgroundImage: 'url(../../img/loginPageBackground.jpg)',
+    backgroundSize: 'cover',
+    overflow: 'hidden',
+  },
+};
+
 
 
 class Login extends Component {
   render() {
     return (
-      <div className="app flex-row align-items-center">
+      <div style={styles.customBackgroundImage} className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
-            <Col md="8">
+            <Col md="4">
               <CardGroup className="mb-0">
-                <Card className="p-4">
-                  <CardBlock className="card-body">
-                    <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon><i className="icon-user"></i></InputGroupAddon>
-                      <Input type="text" placeholder="Username"/>
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <InputGroupAddon><i className="icon-lock"></i></InputGroupAddon>
-                      <Input type="password" placeholder="Password"/>
-                    </InputGroup>
+                <Card className="text-white bg-primary py-5 d-md-down-none">
+                  <CardBlock className="card-body text-center">
+
+
+                    <h1>ADMIN LOGIN</h1>
+                    <TextField
+                          floatingLabelText="EMAIL"
+                          floatingLabelStyle={styles.floatingLabelStyle}
+                          floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+                          underlineFocusStyle={styles.underlineStyle}
+                          floatingLabelFixed={true}
+                          errorStyle={styles.errorStyle}
+                          type="email"
+                        />
+
+                    <TextField
+                         floatingLabelText="PASSWORD"
+                         floatingLabelStyle={styles.floatingLabelStyle}
+                         floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+                         floatingLabelFixed={true}
+                         underlineFocusStyle={styles.underlineStyle}
+                         type="password"
+                       />
                     <Row>
-                      <Col xs="6">
-                        <Button color="primary" className="px-4">Login</Button>
+                      <Col xs="5">
+                        <Button style={styles.linkStyle} color="primary" className="px-3">Forgot password?</Button>
                       </Col>
-                      <Col xs="6" className="text-right">
-                        <Button color="link" className="px-0">Forgot password?</Button>
+                      <Col xs="5" className="text-right">
+                        <Button style={styles.linkStyle} color="primary" className="px-3">Go back to home</Button>
                       </Col>
                     </Row>
                   </CardBlock>
-                </Card>
-                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
-                  <CardBlock className="card-body text-center">
-                    <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                      <Button color="primary" className="mt-3" active>Register Now!</Button>
-                    </div>
-                  </CardBlock>
+
+                  <Row>
+                    <Col sm={{ size: 'auto', offset: 1 }}>
+                          <RaisedButton label="Login" primary={true} style={styles.loginButtonStyle} />
+                    </Col>
+                  </Row>
+
                 </Card>
               </CardGroup>
             </Col>
